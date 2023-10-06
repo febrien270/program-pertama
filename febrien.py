b5449e552eef31260247f0a07b311175c192769a
febrien270 <147120551+febrien270@users.noreply.github.com>
2023-10-05 20:35:54 -0700

    Add files via upload

---
+++ febrien.py
@@ -0,0 +1,6 @@
+
+print('nama saya febrien')
+print('alamat saya jl.peleburan gg.kane perumahan permai')
+print('hobby saya nonton anime main game baca komik dan novel')
+print('saya berasal dari sekolahman 1 bangka barat')
+print('saya lahir pada tanggal 27 februari 2005')
