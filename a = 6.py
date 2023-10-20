89c5cd630a66d75d5e49038f14d767099f0cca21
febrien270 <147120551+febrien270@users.noreply.github.com>
2023-10-20 08:21:25 -0700

    Add files via upload

---
+++ a = 6.py
@@ -0,0 +1,5 @@
+# variable a mempresentasikan panjang
+a = 635654322345667
+b = 5678654322467 # variable b mempersentasikan tinggi
+c = a * b 
+print(c)
---
+++ a = Madura.py
@@ -0,0 +1,4 @@
+a = 'Madura'
+b = 100
+print(type(a))
+print(type(b))
